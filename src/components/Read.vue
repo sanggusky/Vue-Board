51489713443623f2a39c6e33f51244781dc12d5d
sanggusky <sabggusky@hotmail.com>
2019-08-12 10:39:40 +0900

    first commit

---
+++ src/components/Read.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+       <table>
+        <tr>
+            <td>글쓴이</td>
+            <td>제목</td>
+            <td>내용</td>
+        </tr>
+        <tr :key="index" v-for="(value, index) in data" @click="detail(index)">
+            <!--배열을 기반으로 리스트를 렌더링 할 수 있다-->           
+            <td>{{value.writer}}</td>
+            <td>{{value.title}}</td>
+            <td>{{value.content}}</td>
+        </tr>
+       </table>
+       <button @click="write">글쓰기</button>
+    </div>
+</template>
+<script>
+import data from '@/data'
+
+export default {
+    name: 'Read',
+    data() {
+        return {
+            data: data
+        }
+    },
+    methods: {
+        write() {
+            this.$router.push({
+                path: 'create'
+             })
+        },
+        detail(index) {
+            this.$router.push({
+                name: 'Detail',
+                params: {
+                    contentId: index
+                }
+            })
+        }
+    }    
+}
+</script>
---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+//memory에 DB를 저장
+//TEST data
+export default [
+    {
+        writer: '글쓴이1',
+        title: '제목1',
+        content: '내용1'  
+    },
+    {
+        writer: '글쓴이2',
+        title: '제목2',
+        content: '내용2'  
+    },
+    {
+        writer: '글쓴이3',
+        title: '제목3',
+        content: '내용3'  
+    }
+]
+
